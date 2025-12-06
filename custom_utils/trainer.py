510fdb3f4d147c14df0790a1573d44874de5cddf
SauersML <123714540+SauersML@users.noreply.github.com>
2025-12-05 21:49:25 -0600

    Update trainer.py

--- custom_utils/trainer.py
+++ custom_utils/trainer.py
@@ -13,7 +13,7 @@ from typing import Any, Mapping, MutableMapping, Sequence
 
 import numpy as np
 
-from tinker_cookbook import get_renderer as tinker_get_renderer, get_tokenizer as tinker_get_tokenizer
+from tinker_cookbook.infrastructure import get_renderer as tinker_get_renderer, get_tokenizer as tinker_get_tokenizer
 from tinker_cookbook.completers import TinkerTokenCompleter
 
 try:  # Python 3.11+
