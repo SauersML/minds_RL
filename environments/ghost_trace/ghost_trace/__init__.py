06fb5aeab8a1d00b8bb25d7e0685f6fe2f1609a2
User <user@example.com>
2025-12-13 22:57:26 -0600

    Export GhostTraceEnv, GhostTraceParser, _build_rubric from ghost_trace
    Needed for eval_ghost_ood.py to import these classes

--- environments/ghost_trace/ghost_trace/__init__.py
+++ environments/ghost_trace/ghost_trace/__init__.py
@@ -1,3 +1,3 @@
-from .ghost_trace import load_environment
+from .ghost_trace import load_environment, GhostTraceEnv, GhostTraceParser, _build_rubric
 
-__all__ = ["load_environment"]
+__all__ = ["load_environment", "GhostTraceEnv", "GhostTraceParser", "_build_rubric"]
